618410fd0a32dd67e5b04a51ee34abe799d4d4b5
brunoabreusz <bruno.souza2001@gmail.com>
2023-01-11 23:27:27 -0300

    basic type for typescript

---
+++ basicTypes.ts
@@ -0,0 +1,12 @@
+let number: number;
+number = 1;
+
+let string: string;
+string = 'hellow'
+
+let active = true; // boolean
+
+enum ROLES {ADMIN, CEO}
+// ROLES.ADMIN ou CEO
+
+let arrays: string[] = ["Oi"];
